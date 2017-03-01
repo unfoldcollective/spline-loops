34e9f265b12579782f83270dc32664c8628a6946
Philo van Kemenade <philovankemenade@gmail.com>
2017-03-01 14:57:36 +0100

    adjust spline settings for sensorium composition

--- sketch.js
+++ sketch.js
@@ -10,28 +10,62 @@ function setup() {
     var purple = color('#796CAF');
     var green = color('#9CFCCF');
     var colorCombo1 = [pink, teal];
-    var colorCombo2 = [pink, purple];
-    var colorCombo3 = [green, teal];
+    var colorCombo2 = [green, teal];
+    var colorCombo3 = [pink, purple];
 
     splineSettings1 = {
         nPoints: 10,
         origin: {
-            x: 0.5 * width,
-            y: 0.5 * height
+            x: 0.33 * width,
+            y: 0.6 * height
         },
-        radius: 200,
+        radius: 450,
         distortFactor: 80,
         colors: colorCombo1,
         interpolationSteps: 4,
-        moveFactor: 0.4
+        moveFactor: 0.2
     }
-    splineLoop = new SplineLoop(splineSettings1);
+    splineLoop1 = new SplineLoop(splineSettings1);
+
+    splineSettings2 = {
+        nPoints: 10,
+        origin: {
+            x: 0.66 * width,
+            y: 0.6 * height
+        },
+        radius: 350,
+        distortFactor: 80,
+        colors: colorCombo2,
+        interpolationSteps: 4,
+        moveFactor: 0.2
+    }
+    splineLoop2 = new SplineLoop(splineSettings2);
+
+    splineSettings3 = {
+        nPoints: 10,
+        origin: {
+            x: 0.5 * width,
+            y: 0.4 * height
+        },
+        radius: 400,
+        distortFactor: 80,
+        colors: colorCombo3,
+        interpolationSteps: 4,
+        moveFactor: 0.2
+    }
+    splineLoop3 = new SplineLoop(splineSettings3);
 }
 
 function draw() {
     background(0);
-    splineLoop.update();
-    splineLoop.drawSplines();
+    splineLoop1.update();
+    splineLoop1.drawSplines();
+
+    splineLoop2.update();
+    splineLoop2.drawSplines();
+
+    splineLoop3.update();
+    splineLoop3.drawSplines();
 
 }
 
